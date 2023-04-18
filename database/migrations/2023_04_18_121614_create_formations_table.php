d245528e0143efe7fca485846b1be25911f20011
thimothee_lepetz <thimothee_lepetz@ens.univ-artois.fr>
2023-04-18 15:28:23 +0200

    creation migration model seeder de formation

---
+++ database/migrations/2023_04_18_121614_create_formations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('formations', function (Blueprint $table) {
+            $table->id();
+            $table->string('organisme');
+            $table->string('telephone');
+            $table->string('email');
+            $table->string('numero_declaration_existence');
+            $table->bigInteger('siret')->unique();
+            $table->string('adresse');
+            $table->string('interlocuteur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('formations');
+    }
+};
