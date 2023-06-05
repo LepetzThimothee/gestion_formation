40f245b28519c65ea0aafee5d017d6b1775acfcc
thimothee_lepetz <thimothee_lepetz@ens.univ-artois.fr>
2023-06-05 08:50:18 +0200

    edition et suppression formation

--- resources/views/formations/index.blade.php
+++ resources/views/formations/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Liste des Formations</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('style.css')}}">
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #1e285d">
@@ -14,6 +15,16 @@
 </nav>
 <div class="mx-4 mt-4">
     <h1>Liste des Formations</h1>
+    @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if(session('error'))
+        <div class="alert alert-danger mb-1 mt-1">
+            {{ session('error') }}
+        </div>
+    @endif
     <a href="{{ route('formations.create') }}">Vous ne trouvez pas une formation ? Créez-la !</a>
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-sm table-hover clickable-row">
@@ -26,6 +37,8 @@
                 <th>Numéro de SIRET</th>
                 <th>Adresse</th>
                 <th>Interlocuteur</th>
+                <th>Edition</th>
+                <th>Suppression</th>
             </tr>
             </thead>
             <tbody>
@@ -38,6 +51,16 @@
                     <td>{{ $formation->siret }}</td>
                     <td>{{ $formation->adresse }}</td>
                     <td>{{ $formation->interlocuteur }}</td>
+                    <td>
+                        <a href="{{ route('formations.edit', $formation->id) }}" class="btn btn-edit btn-sm mr-2">
+                            Modifier
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{ route('formations.show', $formation->id) }}" class="btn btn-delete btn-sm">
+                            Supprimer
+                        </a>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
---
+++ resources/views/formations/show.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Informations de l'organisme de formation</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #1e285d">
+    <!-- Logo et lien vers l'accueil -->
+    <a class="navbar-brand" href="/">Gestion de formation</a>
+</nav>
+<div class="container mt-4">
+    <h1>Informations de l'organisme de formation</h1>
+    @if(session('status'))
+        <div class="alert alert-success mt-4">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if(session('error'))
+        <div class="alert alert-danger mt-4">
+            {{ session('error') }}
+        </div>
+    @endif
+    <div class="card mt-4">
+        <div class="card-body">
+            <h5 class="card-title">Organisme de formation</h5>
+            <table class="table table-bordered">
+                <tbody>
+                <tr>
+                    <th scope="row">Organisme</th>
+                    <td>{{ $formation->organisme }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Coordonnées téléphoniques</th>
+                    <td>{{ $formation->telephone }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Adresse email</th>
+                    <td>{{ $formation->email }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Numéro déclaration existence</th>
+                    <td>{{ $formation->numero_declaration_existence }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Numéro de SIRET</th>
+                    <td>{{ $formation->siret }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Adresse</th>
+                    <td>{{ $formation->adresse }}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Interlocuteur</th>
+                    <td>{{ $formation->interlocuteur }}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="text-right">
+                <a href="{{ route('formations.edit', $formation->id) }}" class="btn btn-primary">Modifier</a>
+                <form id="deleteForm" action="{{ route('formations.destroy', $formation->id) }}" method="POST" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Supprimer</button>
+                </form>
+                <a href="{{ route('formations.index') }}" class="btn btn-secondary">Annuler</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
